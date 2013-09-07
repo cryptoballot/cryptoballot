e45c9081341167d100552dc9180b9059145461ac
phayes <patrick.d.hayes@gmail.com>
2013-09-06 23:14:45 -0700

    Adding tests, run script, and test / example config

--- ballot.go
+++ ballot.go
@@ -1,14 +1,6 @@
 package main
 
-// NOTES
-// See https://bitbucket.org/bumble/bumble-golang-common/src/master/key/publickey.go
-
 import (
-	"crypto"
-	"crypto/rsa"
-	"crypto/sha512"
-	"crypto/x509"
-	"encoding/base64"
 	"encoding/hex"
 	"errors"
 )
@@ -36,90 +28,11 @@ func NewBallotID(bidstr string) (BallotID, error) {
 	return BallotID(bidstr), nil
 }
 
-type PublicKey string
-
-// Given a string, return a new PublicKey object.
-// This function also performs error checking to make sure the key is valid.
-// @@TODO: Reject keys that are under 2048 bits in size
-func NewPublicKey(pkstr string) (PublicKey, error) {
-	if pkstr == "" {
-		return "", errors.New("No public key provided")
-	}
-
-	pk := PublicKey(pkstr)
-
-	if _, err := pk.getCryptoKey(); err != nil {
-		return "", err
-	}
-
-	return pk, nil
-}
-
-// Extract the raw bytes out of the base64 encoded public key
-func (pk PublicKey) GetBytes() ([]byte, error) {
-	return base64.StdEncoding.DecodeString(string(pk))
-}
-
-// Parse the PublicKey (which is stored as a base64 string) into a rsa.PublicKey object, ready to be used for crypto functions
-func (pk PublicKey) getCryptoKey() (*rsa.PublicKey, error) {
-	rawpk, err := pk.GetBytes()
-	if err != nil {
-		return nil, err
-	}
-	pubkey, err := x509.ParsePKIXPublicKey(rawpk)
-	if err != nil {
-		return nil, err
-	}
-	return pubkey.(*rsa.PublicKey), nil
-}
-
-// Get the corresponding BallotID, which is the (hex encoded) SHA512 of the (base64 encoded) public key.
-func (pk PublicKey) GetBallotID() BallotID {
-	// @@TODO this can be more direct in Go 1.2
-	h := sha512.New()
-	h.Write([]byte(pk))
-	return BallotID(hex.EncodeToString(h.Sum(nil)))
-}
-
 type Vote []string // Ordered list of choices represented by git addresses
-
-type TagSet []Tag
 
 type Tag struct {
 	Key   string
 	Value string
 }
 
-type Signature string
-
-func NewSignature(sigstr string) (Signature, error) {
-	if sigstr == "" {
-		return "", errors.New("Signature not provided")
-	}
-	sig := Signature(sigstr)
-	if _, err := sig.GetBytes(); err != nil {
-		return "", err
-	}
-	return sig, nil
-}
-
-// Extract the raw bytes out of the base64 encoded signature
-func (sig Signature) GetBytes() ([]byte, error) {
-	return base64.StdEncoding.DecodeString(string(sig))
-}
-
-func (sig Signature) VerifySignature(pk PublicKey, message []byte) error {
-	sigBytes, err := sig.GetBytes()
-	if err != nil {
-		return err
-	}
-
-	pubkey, err := pk.getCryptoKey()
-	if err != nil {
-		return err
-	}
-
-	hash := sha512.New()
-	hash.Write(message)
-	return rsa.VerifyPKCS1v15(pubkey, crypto.SHA512, hash.Sum(nil), sigBytes)
-}
+type TagSet []Tag
---
+++ crypto_test.go
@@ -0,0 +1,101 @@
+package main
+
+import "testing"
+
+var (
+	goodKey = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCuf2fxzp6UI2ejqeCf2vtj6k4MpNak4RSo1K02b+5Oi1WPVe6xZjNLDYDm6u6KpgSEhdYyVgigyknKfHALcQLwKbAP79RAmP3xwpv8+ts1r3rYBxooeRV50AXL9AuTb6qSnVHQ2LbixcgAvq+IpHqb6f9IhQLFhTQbCy/6LS1NQQIDAQAB"
+	goodSig = "Nz2xr8Ibn3CPwUrV2Ptr2iCtdtBEzrQ/10vTPlb8KfLF6R4JRryj7g1AlP75l1DwRjuSm90j2MTaNXgFceIQAUOPDvkOubyY7tyvTLY9LxRqw2iynoVZB79KJ2mZ9a2K9811mfQPpCqW0kxXMoFX3svza5arqAvFrwHNjM/K5YE="
+	goodSHA = "1d6d8c6965c4a72c35c6bf9ac66483405168578ee503bf4b4a2248b3cd0e2415d9fa2436eab027635819fdc4d458551081b8e0039ab242b08ba7c664633fe923"
+	goodMes = "PUT /vote/12345/" + goodSHA
+	badKey  = "IAMNOTAKEY"
+	badSig  = "IAMNOTAVALIDSIGNATURE"
+	badSHA  = "GoodForNothingSHA"
+	badMes  = "GET /bad/message"
+)
+
+func TestGoodPublicKey(t *testing.T) {
+	pk, err := NewPublicKey(goodKey)
+	if err != nil {
+		t.Error(err)
+	}
+
+	_, err = pk.GetBytes()
+	if err != nil {
+		t.Error(err)
+	}
+
+	_, err = pk.getCryptoKey()
+	if err != nil {
+		t.Error(err)
+	}
+
+	ballotID := pk.GetBallotID()
+	if string(ballotID) != goodSHA {
+		t.Errorf("BallotID does not match SHA512")
+	}
+}
+
+func TestBadPublicKey(t *testing.T) {
+	pk, err := NewPublicKey(badKey)
+	if err == nil {
+		t.Errorf("Invalid public key did not return error")
+	}
+	if string(pk) != "" {
+		t.Errorf("Invalid public should return empty string")
+	}
+}
+
+func TestGoodSignature(t *testing.T) {
+	sig, err := NewSignature(goodSig)
+	if err != nil {
+		t.Error(err)
+	}
+
+	_, err = sig.GetBytes()
+	if err != nil {
+		t.Error(err)
+	}
+
+	goodpk, _ := NewPublicKey(goodKey)
+	err = sig.VerifySignature(goodpk, []byte(goodMes))
+	if err != nil {
+		t.Error(err)
+	}
+	err = sig.VerifySignature(goodpk, []byte(badMes))
+	if err == nil {
+		t.Errorf("Verified bad message as valid")
+	}
+
+	badpk, _ := NewPublicKey(badKey)
+	err = sig.VerifySignature(badpk, []byte(goodMes))
+	if err == nil {
+		t.Errorf("Verified bad public key as valid")
+	}
+	err = sig.VerifySignature(badpk, []byte(badMes))
+	if err == nil {
+		t.Errorf("Verified bad public key and bad message as valid")
+	}
+}
+
+func TestBadSignature(t *testing.T) {
+	sig, err := NewSignature(badSig)
+	if err == nil {
+		t.Errorf("Invalid signature did not throw error")
+	}
+
+	if string(sig) != "" {
+		t.Errorf("Invalid signature should return empty string")
+	}
+
+	goodpk, _ := NewPublicKey(goodKey)
+	err = sig.VerifySignature(goodpk, []byte(goodMes))
+	if err == nil {
+		t.Errorf("Invalid signature should not be able to verify")
+	}
+
+	emptypk, _ := NewPublicKey("")
+	err = sig.VerifySignature(emptypk, []byte{})
+	if err == nil {
+		t.Errorf("Invalid signature should not be able to an empty public-key")
+	}
+}
