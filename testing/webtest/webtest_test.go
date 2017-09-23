9495ce87f645d2fae4ed960c56185d101bdea387
phayes <patrick.d.hayes@gmail.com>
2017-09-22 19:33:22 -0700

    Fixing location of common functions

--- testing/webtest/webtest_test.go
+++ testing/webtest/webtest_test.go
@@ -1,19 +1,8 @@
 package webtest
 
 import (
-	"fmt"
-	"log"
-	"os"
-	"os/exec"
-	"sync"
 	"testing"
 	"time"
-)
-
-var (
-	electionclerkCmd *exec.Cmd
-	ballotboxCmd     *exec.Cmd
-	once             sync.Once
 )
 
 // TestWebElection compiles and runs electionclerk and ballotbox, then tests a small election using botyh
@@ -45,72 +34,4 @@ func TestWebElection(m *testing.T) {
 
 	// We're done
 	Success()
-}
-
-// Fail cleans up then calls log.Fatal
-func Fail(v ...interface{}) {
-	once.Do(func() {
-		Cleanup()
-		log.Fatal(v...)
-	})
-}
-
-// Success is called when we've successfully completed the test
-func Success() {
-	time.Sleep(2 * time.Second) // Give running processes a little time to fail before we declare victory
-	fmt.Println("Success!")
-	once.Do(func() {
-		Cleanup()
-	})
-}
-
-// Cleanup kills electionclerk and ballotbox processes, deletes databases, and removes compiled binaries.
-func Cleanup() {
-	// Kill long-running processes
-	if electionclerkCmd != nil {
-		electionclerkCmd.Process.Kill()
-	}
-	if ballotboxCmd != nil {
-		ballotboxCmd.Process.Kill()
-	}
-
-	// Run the cleanup script
-	cmd := exec.Command("sh", "cleanup.sh")
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	err := cmd.Run()
-	if err != nil {
-		log.Print(err)
-	}
-}
-
-// Run a command, piping the results to stderr and stdout.
-// Fatally fail if the command fails in any way
-// This function will return before the command finishes running
-func runCommand(name string, args ...string) *exec.Cmd {
-	cmd := exec.Command(name, args...)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	err := cmd.Start()
-	if err != nil {
-		Fail(err)
-	}
-	go func() {
-		err = cmd.Wait()
-		if err != nil {
-			Fail(err)
-		}
-	}()
-	return cmd
-}
-
-// runCommandSync runs a command syncronously, waiting until it is done.
-func runCommandSync(name string, args ...string) {
-	cmd := exec.Command(name, args...)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	err := cmd.Run()
-	if err != nil {
-		Fail(err)
-	}
 }
