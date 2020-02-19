88f7d7b62faf856ffea8af870ab7b18aab31b760
phayes <patrick.d.hayes@gmail.com>
2020-02-18 22:37:18 -0800

    Spelling

--- cryptoballot/src/lib.rs
+++ cryptoballot/src/lib.rs
@@ -2,16 +2,16 @@
 //!
 //! It is currenly under active development and is not production ready.
 //!
-//! CryptoBallot is fundementally a transaction processor and validator, that when taken together creates an end-to-end verifiable voting system.
+//! CryptoBallot is fundamentally a transaction processor and validator. When transactions are validated in order, it creates an end-to-end verifiable voting system.
 //!
 //! ## Glossary:
 //!  - **Transaction 1: Election Transaction** - Defines an election, created by an election authority.
 //!  - **Transaction 2: Vote Transaction** - Posted by a voter to cast a vote in an election.
 //!  - **Transaction 3: Secret Share Transaction** - Posted by a trustee to allow votes to be decrypted and viewed.
 //!  - **Transaction 4: Decryption Transaction** - Decrypt a vote, allowing it to be tallied.
-//!  - **Election Authority** - Creates an Election Transaction and distributes encryption secret to trustees via Shamir Secret Sharing.
-//!  - **Trustee** - Holds a vote decryption secret share, posts Secret Share Transactions.
-//!  - **Authenticator** - Certified that a voter can vote an election and ballot using blind-singing.
+//!  - **Election Authority** - Creates an Election Transaction and distributes the encryption secret to trustees via Shamir Secret Sharing.
+//!  - **Trustee** - Holds a vote-decryption secret share, posts Secret Share Transactions.
+//!  - **Authenticator** - Certifies that a voter can vote an election and ballot using blind-signing.
 //!  - **Ballot** - A set of contests for an election, usually restricted to a geographic area.
 
 #[macro_use]
