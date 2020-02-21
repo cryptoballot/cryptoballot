812aca676655a585ef019e029e407df0df2cb0ad
phayes <patrick.d.hayes@gmail.com>
2020-02-21 10:49:18 -0800

    Adding tally to election test

--- test_elections/run_tests.bash
+++ test_elections/run_tests.bash
@@ -5,3 +5,4 @@ cargo run --bin="cryptoballot_cli" -- post test_election_1/transaction_2.vote.js
 cargo run --bin="cryptoballot_cli" -- post test_election_1/transaction_3.secret_share.json
 cargo run --bin="cryptoballot_cli" -- post test_election_1/transaction_4.secret_share.json
 cargo run --bin="cryptoballot_cli" -- post test_election_1/transaction_5.decryption.json
+cargo run --bin="cryptoballot_cli" -- tally bebb7c9de7d7d77ec3c4f3fbc73b0d0100000000000000000000000000000000
